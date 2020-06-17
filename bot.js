6d9b1a1f2b5b40fbc13e6aea3d41bc39799158b7
Joao Koritar <jakoritarleite@gmail.com>
2020-06-17 06:52:02 +0000

    update: added PREFIX and new command.

--- bot.js
+++ bot.js
@@ -3,8 +3,16 @@ const client = new Discord.Client();
 
 client.on('ready', () => { console.log('I am ready!') });
 client.on('message', message => {
-	if (message.content === 'ping') {
-		message.reply('pong');
+	if (!message.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+	const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+	const args = msg.content.split(' ').slice(1).join(' ');
+
+	if (command === 'ping') {
+		message.reply('pong :ping_pong:');
+	}
+
+	else if (command === 'hello') {
+		message.reply('Hi ?');
 	}
 });
 
