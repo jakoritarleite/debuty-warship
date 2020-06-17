18985010e4d5621f13321f4740bd9ba6d295e08b
Joao Koritar <jakoritarleite@gmail.com>
2020-06-17 06:54:22 +0000

    fix: variable name does not exists.

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 
 client.on('ready', () => { console.log('I am ready!') });
 client.on('message', message => {
-	if (!message.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-	const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-	const args = msg.content.split(' ').slice(1).join(' ');
+	if (!message.content.startsWith(process.env.PREFIX) || !message.guild) return;
+	const command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
+	const args = message.content.split(' ').slice(1).join(' ');
 
 	if (command === 'ping') {
 		message.reply('pong :ping_pong:');
