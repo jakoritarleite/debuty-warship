dae0fc43825014f54c29c533847ef1d7296d941f
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 23:03:57 +0000

    fix: syntax error on returning

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ DClient.on('message', async message => {
     const Command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
     const Arguments = message.content.split(' ').slice(1).join(' ');
 
-    Command ? await Main.main(message, Command, Arguments) : return null;
+    Command ? await Main.main(message, Command, Arguments) : null;
 });
 
 DClient.login(process.env.BOT_TOKEN);
