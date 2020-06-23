23e479d4903f7a01345fd83413dcdc97da3a75e2
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 23:11:30 +0000

    add: await to function

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const Music = require('./robots/music');
 
 const Commands = {
     ping: Util.ping,
+    hello: Util.hello,
     join: Music.join,
     play: Music.play
 }
 
 module.exports = {
     main: async function (message, command, args) {
-        args ? Commands[command](message, args) : Commands[command](message);
+        args ? await Commands[command](message, args) : await Commands[command](message);
     }
 }
