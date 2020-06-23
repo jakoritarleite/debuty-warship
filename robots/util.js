896d44ea0b6cf67f923e57cf63b27ab5056e181c
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 23:21:04 +0000

    add: simple logging

--- robots/util.js
+++ robots/util.js
@@ -3,8 +3,10 @@ const { Message } = require("discord.js")
 module.exports = {
     ping: async function (message, args) {
         message.reply('Pong :ping_pong:');
+        console.log('Ping` to user');
     },
     hello: async function (message, args) {
         message.reply('Hi');
+        console.log('Saying hi to user');
     }
 }
