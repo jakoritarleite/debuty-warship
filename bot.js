aba4e8881a242b5b48364071090aa5963f3b22d0
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 23:01:22 +0000

    add: ternary condition

--- bot.js
+++ bot.js
@@ -8,9 +8,7 @@ DClient.on('message', async message => {
     const Command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
     const Arguments = message.content.split(' ').slice(1).join(' ');
 
-    if (Command) {
-        await Main.main(Command, Arguments);
-    }
+    Command ? await Main.main(message, Command, Arguments) : return null;
 });
 
 DClient.login(process.env.BOT_TOKEN);
