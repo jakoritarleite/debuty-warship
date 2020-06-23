224dd1de3062afce44a6ffad9cd2736acf686e81
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 22:10:27 +0000

    add: ternay condition

--- bot.js
+++ bot.js
@@ -13,9 +13,7 @@ DClient.on('message', async message => {
     const Command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
     const Arguments = message.content.split(' ').slice(1).join(' ');
 
-    if (Command) {
-        await Robot.main(Command, Arguments);
-    }
+    Command ? await Robot.main(Command, Arguments) : null;
 });
 
 DClient.login(process.env.BOT_TOKEN);
