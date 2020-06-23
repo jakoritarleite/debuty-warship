5b277b148edbc58564e99127d70728e15aac76ab
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 21:49:11 +0000

    fix: Client variable in conflict with Client class

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const { Client, Message } = require('discord.js');
-const Client = new Client();
+const DClient = new Client();
 
 const Robot = {
     main: require('./main.js');
 }
 
-Client.on('ready', () => { console.log('I am ready!') });
+DClient.on('ready', () => { console.log('I am ready!') });
 
-Client.on('message', async message => {
+DClient.on('message', async message => {
     const Command = message.content.split(' ')[0].substr(process.env.PREFIX.length);
     const Arguments = message.content.split(' ').slice(1).join(' ');
 
