88880c9b9ba60be3eb3f48105ba7bf7b26d32787
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 23:00:17 +0000

    add: main music bot

---
+++ robots/music.js
@@ -0,0 +1,8 @@
+module.exports = {
+    join: async function (message, args) {
+        console.log('Joining channel');
+    },
+    play: async function (message, args) {
+        console.log('Playing music');
+    }
+}
