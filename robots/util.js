c94fba48d5ba4f5e845f1e43610ac9e95a6b4304
Joao Koritar <jakoritarleite@gmail.com>
2020-06-23 23:15:33 +0000

    add: args to f() arguments

--- robots/util.js
+++ robots/util.js
@@ -1,10 +1,10 @@
 const { Message } = require("discord.js")
 
 module.exports = {
-    ping: async function (message) {
+    ping: async function (message, args) {
         message.reply('Pong :ping_pong:');
     },
-    hello: async function (message) {
+    hello: async function (message, args) {
         message.reply('Hi');
     }
 }
