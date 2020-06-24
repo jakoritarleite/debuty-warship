1bb206d3b0e86d6c69dc963f4ccd5a0114a82308
Joao Koritar <jakoritarleite@gmail.com>
2020-06-24 00:07:55 +0000

    add: experimental react function

--- robots/music.js
+++ robots/music.js
@@ -2,5 +2,6 @@ export async function join(message, args) {
     console.log('Joining channel');
 }
 export async function play(message, args) {
+    message.react(':thumbsup:');
     console.log('Playing music');
 }
