1843685afecfb2ba74cac9acf20111f171e6f48f
Joao Koritar <jakoritarleite@gmail.com>
2020-06-24 00:11:09 +0000

    rm: CommonJS

--- robots/music.js
+++
@@ -1,7 +0,0 @@
-export async function join(message, args) {
-    console.log('Joining channel');
-}
-export async function play(message, args) {
-    message.react(':thumbsup:');
-    console.log('Playing music');
-}
--- robots/util.js
+++
@@ -1,8 +0,0 @@
-export async function ping(message, args) {
-    message.reply('Pong :ping_pong:');
-    console.log('Ping` to user');
-}
-export async function hello(message, args) {
-    message.reply('Hi');
-    console.log('Saying hi to user');
-}
